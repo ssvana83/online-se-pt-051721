a645fe72725fdce042518a6a6a69827af1a75627
brewchetta <chett.tiller@gmail.com>
2021-06-15 17:24:56 -0400

    added 07 lecture

---
+++ 07-using-fetch-pt-1/scripts/index.js
@@ -0,0 +1,9 @@
+// We have an empty array ... for now
+let catArray = []
+
+// Various elements we'll need
+const imgContainer = document.querySelector('#images-cattainer')
+const loadButton = document.querySelector('#load-cats-button')
+const newCatForm = document.querySelector('#new-cat-form')
+
+// So now it's time to ... fetch!
