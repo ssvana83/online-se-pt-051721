364a190223b01dcce9a53a21dba2d78d3fcc9af4
brewchetta <chett.tiller@gmail.com>
2021-06-15 19:03:01 -0400

    fetch

--- 07-using-fetch-pt-1/scripts/index.js
+++ 07-using-fetch-pt-1/scripts/index.js
@@ -2,8 +2,41 @@
 let catArray = []
 
 // Various elements we'll need
-const imgContainer = document.querySelector('#images-cattainer')
+const cattainer = document.querySelector('#images-cattainer')
 const loadButton = document.querySelector('#load-cats-button')
 const newCatForm = document.querySelector('#new-cat-form')
 
 // So now it's time to ... fetch!
+
+loadButton.addEventListener("click", () => {
+  fetch('http://localhost:3000/cats')
+  .then(res => res.json())
+  .then(parsedJS => {
+    console.log(parsedJS.forEach(cat => {
+      const img = document.createElement("img")
+      img.src = cat.img_url
+      cattainer.appendChild(img)
+    }))
+  })
+})
+
+// 1. Bread
+// 2. Spaghetti
+// 3. Presciption --> ??? mins --> async
+// 4. Ice cream
+
+// const
+//
+// const
+//
+// function
+//
+// async --> we don't want this to block our other code
+//
+// function
+//
+// function
+//
+// const
+//
+// --> async -> is it ready?
